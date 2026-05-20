5e59f3e997445e7c8b882d6e7c2421ce4e542620
Noah Jutz <noah@noahjutz.com>
2026-05-20 10:27:22 +0200

    Add Containerfile to build all notes

---
+++ Containerfile
@@ -0,0 +1,9 @@
+FROM docker.io/123marvin123/typst:0.14.2
+
+WORKDIR /app
+
+RUN mkdir -p /output
+
+COPY . .
+
+CMD ["bash", "-c", "./build.sh /output; tree"]
---
+++ build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+OUTPUT=$1
+
+TARGET_DIRS=(
+    "CG"
+    "CR"
+    "DW"
+    "OR"
+    "OS"
+)
+
+for dir in "${TARGET_DIRS[@]}"; do
+    file="${dir}/main.typ"
+
+    if [ ! -f "$file" ]; then
+        exit 1
+    fi
+
+    typst compile "$file" "$OUTPUT/${dir}.pdf" \
+        --font-path . \
+        --ignore-system-fonts \
+        --root .
+done
